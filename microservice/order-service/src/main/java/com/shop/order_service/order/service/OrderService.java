c88cf175fdb6968886635abe38e3c834d28a78da
Kübra Şişman <87641588+kubrasisman@users.noreply.github.com>
2025-10-26 18:13:26 +0300

    Updated OrderService.java

--- microservice/order-service/src/main/java/com/shop/order_service/order/service/OrderService.java
+++ microservice/order-service/src/main/java/com/shop/order_service/order/service/OrderService.java
@@ -1,4 +1,19 @@
 package com.shop.order_service.order.service;
 
+import com.shop.order_service.order.dto.OrderData;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.List;
+
 public interface OrderService {
+
+    OrderData placeOrder(HttpSession session);
+
+    OrderData getOrderByCode(String code);
+
+    List<OrderData> getOrdersByCustomerEmail(String email);
+
+    List<OrderData> getAllOrders();
+
+    OrderData updateOrderStatus(String code, String status);
 }
